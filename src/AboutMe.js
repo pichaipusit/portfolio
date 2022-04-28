4c1e17a34a432b76d410b7f22bd827ad37876554
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2022-04-28 15:47:47 +0700

    chnage info about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -17,16 +17,13 @@ function AboutMe() {
           <span style={{ marginLeft: "1rem" }}>I'M </span>
           <span className="name">PICHAIPUSIT </span> KAMSORN
         </h2>
-        <p className="role">
-          A FULL STACK DEVELOPER
-          <span style={{ opacity: 0.5 }}>(trying to be)</span>
-        </p>
+        <p className="role">A FULL STACK DEVELOPER</p>
       </div>
       <Typography variant="subtitle1" sx={{ margin: "20px" }}>
         I love to adventure in the Tech business world <br /> because there're
         so many amazing things that drive me crazy, <br /> wanting to know more
         and earn those skills to become a
-        <span className="ceo">Breathtaking CEO!</span>
+        <span className="ceo">Breathtaking DEV!</span>
       </Typography>
       <img
         className={`metal-img ${isMatch ? "metal-xs" : "metal-md"}`}
